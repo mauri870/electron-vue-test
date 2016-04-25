fdb0551e095c9325591a7a5dcabf46af68d99bfc
Mauri de Souza Nunes <mauri870@gmail.com>
2016-04-25 09:13:49 +0800

    Big bang :boom:

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container text-center">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jumbotron">
+    <h1>{{ msg }}</h1>
+    <marquee><p>This is a <b>VueJs</b> + <b>Electron</b> app</p><marquee>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // Note: modifying `msg` below will not cause changes to occur with
+      // hot-reload. As reloaded components preserve their initial state,
+      // modifying these values will have no effect.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
